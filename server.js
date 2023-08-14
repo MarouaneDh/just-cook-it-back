52bbcb2fbfb48ebb4c76e2fa7c2593d697a96380
Marouane Dhambri <marouane.dhambri@agence-inspire.com>
2023-08-14 08:15:05 +0100

    changed mongodb uri

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 require("dotenv").config();
-const path = require("path");
 const dbConnect = require("./config/connectDB");
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -20,10 +19,10 @@ app.use("/api/ingredients", require("./routes/ingredients"));
 app.use("/api/auth", authRouter);
 
 // serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  //set static folder
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   //set static folder
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
